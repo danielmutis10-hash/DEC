f3130c146ba5bcad616bdb17d5cee238a443f74b
Daniel Mutis <daniel.mutis10@gmail.com>
2026-09-18 09:52:46 +0200

    modificación de contenido

--- Ejercicios/ejercicio14.js
+++ Ejercicios/ejercicio14.js
@@ -4,10 +4,20 @@ console.log("\n--- Ejercicio 14 ---");
 //    "nombreMostrado" con "usuario" si existe o "Invitado" si no.
 // TODO
 
+let usuario = null;
+let nombreMostrado = usuario || "invitado";
+
+console.log(nombreMostrado);
 
 // b) Usa && para mostrar un texto SOLO si una variable booleana
 //    "sesionIniciada" es true.
 // TODO
 
+let sesionIniciada = true;
+
+sesionIniciada && console.log("Bienvenido");
+
 // c) Usa ! para invertir el valor de una variable booleana y muÃ©stralo.
 // TODO
+
+console.log(!sesionIniciada);
--- Ejercicios/ejercicio15.js
+++ Ejercicios/ejercicio15.js
@@ -5,10 +5,20 @@ console.log("\n--- Ejercicio 15 ---");
 //    (Â¿te deja hacerlo aunque sea const?). Muestra el objeto final.
 // TODO
 
+const persona1 = {nombre: "Carlos", edad: 20};
+persona1.edad = 21;
+console.log(persona1);
+
 // b) Crea "persona2" con el MISMO contenido que persona1 (otro objeto
 //    distinto) y compara persona1 === persona2. Comenta el resultado.
 // TODO
 
+const persona2 = {nombre: "Carlos", edad: 21};
+console.log(persona1 === persona2);
+
 // c) Crea "persona3 = persona1" (misma referencia) y compara
 //    persona1 === persona3. Comenta la diferencia con el apartado b).
 // TODO
+
+const persona3 = persona1;
+console.log(persona1 === persona3);
